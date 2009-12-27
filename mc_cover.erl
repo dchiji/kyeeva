3bfb80a361a896855210fb6cb2d05029fd363384
CHIJIWA Daiki <daiki@CHIJIWA-Daiki-no-MacBook-Pro.local>
2009-12-27 17:14:17 +0900

    mc_cover.erl

--- mc_cover.erl
+++ mc_cover.erl
@@ -1,3 +1,29 @@
+%%    Copyright 2009  CHIJIWA Daiki <daiki41@gmail.com>
+%%    
+%%    Redistribution and use in source and binary forms, with or without
+%%    modification, are permitted provided that the following conditions
+%%    are met:
+%%    
+%%         1. Redistributions of source code must retain the above copyright
+%%            notice, this list of conditions and the following disclaimer.
+%%    
+%%         2. Redistributions in binary form must reproduce the above copyright
+%%            notice, this list of conditions and the following disclaimer in
+%%            the documentation and/or other materials provided with the
+%%            distribution.
+%%    
+%%    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+%%    "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+%%    LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+%%    FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE FREEBSD
+%%    PROJECT OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+%%    SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+%%    TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR 
+%%    PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF 
+%%    LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING 
+%%    NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS 
+%%    SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
 -module(mc_cover).
 -export([start/0, init/1]).
 -behaviour(supervisor).
@@ -9,6 +35,15 @@ start() ->
     supervisor:start_link({local, ?MODULE}, spd, []).
 
 
+%%====================================================================
+%% supervisor callbacks
+%%====================================================================
+
+%%--------------------------------------------------------------------
+%% Function: init
+%% Description: supervisor:start_linkにより呼び出される．
+%% Returns: {ok,{{RestartStrategy,MaxR,MaxT},[ChildSpec]}}
+%%--------------------------------------------------------------------
 init(_Args) ->
     RestartStrategy = one_for_one,
     MaxR = 1,
@@ -18,6 +53,11 @@ init(_Args) ->
     {ok, {{RestartStrategy, MaxR, MaxT}, ChildSpec}}.
 
 
+%%--------------------------------------------------------------------
+%% Function: childspec
+%% Description: 子プロセスの詳細設定を返す
+%% Returns: {Id,StartFunc,Restart,Shutdown,Type,Modules}
+%%--------------------------------------------------------------------
 childspec() ->
     childspec({?MODULE, mc_cover, []}).
 
@@ -30,6 +70,10 @@ childspec(StartFunc) ->
 
     {ID, StartFunc, Restart, Shutdown, Type, Modules}.
 
+
+%%====================================================================
+%% memcached protocol
+%%====================================================================
 
 mc_cover() ->
     {ok, LSock} = gen_tcp:listen(
