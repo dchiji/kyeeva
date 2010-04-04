57d6fe1d021b660ed6285cefeb7f9d57a0db7766
CHIJIWA Daiki <daiki@em114-51-55-82.pool.e-mobile.ne.jp>
2010-04-04 17:10:31 +0900

    add include/

---
+++ include/common.hrl
@@ -0,0 +1,22 @@
+-record(pstate, {
+        parent_key :: atom() | list(),
+        mvector :: binary(),
+        smaller :: list(tuple()),
+        bigger :: list(tuple())
+    }).
+
+-ifdef(debug).
+    -define(LEVEL_MAX, 4).
+-else.
+    %-define(LEVEL_MAX, 8).
+    -define(LEVEL_MAX, 16).
+    %-define(LEVEL_MAX, 32).
+    %-define(LEVEL_MAX, 64).
+    %-define(LEVEL_MAX, 128).
+-endif.
+
+-ifdef(debug).
+    -define(SERVER_MODULE, mock_sg_join).
+-else.
+    -define(SERVER_MODULE, sg).
+-endif.
